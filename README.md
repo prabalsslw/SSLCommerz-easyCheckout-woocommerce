2544feb683c0d23cc061fe74d89b689fddca704b
Prabal Mallick <prabalsslw@gmail.com>
2020-06-06 23:29:36 +0600

    Initial Release

---
+++ README.md
@@ -0,0 +1,46 @@
+# SSLCommerz easyCheckout for WooCommerce
+
+> WooCommerce plugin for SSLCommerz payment gateway with IPN Support.
+
+## Getting Started
+
+### Prerequisites
+
+1. Wordpress 5.1.*
+2. WooCommerce 3.6.*
+3. cURL php extension.
+
+### Installation
+
+1. Download zip file or Clone the repository.
+2. Unzip if downloaded zip file.
+3. Move the whole directory to ```/wp-content/plugins/```
+4. Activate the plugin through the 'Plugins' menu in admin panel.
+
+**[NOTE]** This plugin will automatically set the IPN url, there is no need to set IPN in merchant panel when using this.
+
+### Configuration
+
+1. Open Admin Panel.
+2. Navigate to ```Woocommerce > Settings > Payments``` tab.
+
+![Payments Menu](images/config1.png)
+
+3. Click on SSLCommerz to edit the settings. If you do not see SSLCommerz in the list at the top of the screen make sure you have activated the plugin in the WordPress Plugin Manager.
+4. Enable the Payment Method, give a proper title and description to show on the checkout page,  fill up stroe id and store passowrd fields carefully, select success and fail/cancel page.
+5. Setup is complete. Check if everything is working properly.
+
+
+## FAQ
+
+### What is WooCommerce?
+> WooCommerce is an open-source e-commerce plugin for WordPress. 
+
+### What is SSLCommerz?
+> SSLCOMMERZ is the first payment gateway in Bangladesh opening doors for merchants to receive payments on the internet via their online stores.
+### What is a Payment Gateway?
+> Payment Gateway is a service that allows merchant to accept secure credit card transactions online. It essentially connects a merchant website to a transaction processor like bank to take payment from a customer for an order.
+
+
+## Contributors
+> Prabal Mallick
---
+++ lib/sslcommerz_ipn.php
@@ -0,0 +1,84 @@
+<?php
+	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+	if(get_option('woocommerce_sslcommerz_settings')!='') {
+		$data=get_option('woocommerce_sslcommerz_settings');
+		if ($data['store_id'] != '' || $data['store_password'] != '') {
+			$store_id = $data['store_id'];
+			$store_passwd = $data['store_password'];
+		} else {
+			die("Invalid or Empty Information ");
+		}
+
+		if($data['testmode'] == 'yes') {
+			$url = "https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php";
+		} else {
+			$url = "https://securepay.sslcommerz.com/validator/api/validationserverAPI.php";
+		}
+	} else {
+		die("SSLCOMMERZ payment gateway is not enabled!");
+	}
+
+	if(isset($_POST['tran_id']) && isset($_POST['val_id']) && (isset($_POST['status']) && $_POST['status'] == "VALID")) 
+	{
+		global $woocommerce;
+		$tran_id = $_POST['tran_id'];
+		$val_id = $_POST['val_id'];
+
+		// echo $orderid = substr($tran_id, 0, strpos($tran_id, '_'));
+
+        $order = new WC_Order($tran_id);
+
+		$requested_url = ($url."?val_id=".$val_id."&Store_Id=".$store_id."&Store_Passwd=".$store_passwd."&v=1&format=json");
+		$handle = curl_init();
+		curl_setopt($handle, CURLOPT_URL, $requested_url);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($handle);
+		$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+		if($code == 200 && !( curl_errno($handle))) 
+		{	
+			$result = json_decode($result);
+	
+			if($order->get_total() == trim($result->currency_amount))
+			{ 
+				if($result->status=='VALIDATED' || $result->status=='VALID') 
+				{ 
+					if($order->get_status() == 'pending')
+					{
+						if($_POST['amount'] != "")
+						{							        
+							$order -> update_status('Processing');
+							$order -> payment_complete();
+							$woocommerce->cart->empty_cart();
+							$result_msg =  "IPN validation success.";
+						}
+						else
+						{
+						    $result_msg=  "IPN: Amount can not be empty!";
+						}
+					}	
+					else
+					{
+						$result_msg=  "IPN: Order already in processing Status!";
+					}
+				}
+				else
+				{
+					 $result_msg=  "IPN: Your Validation id could not be Verified!";
+				}
+			}
+			else
+			{
+				$result_msg= "IPN: Your Paid Amount is Mismatched!";
+			}
+			$order->add_order_note($result_msg);
+			echo $result_msg;
+		}
+	}
+	else{
+		die("No IPN Request Received"); 
+	}
+?>
